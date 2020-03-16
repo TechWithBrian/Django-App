61b65afecab5d0d0dc3912289013a346ef58f863
brianparkerin <brianparkerin@gmail.com>
2020-03-16 14:48:38 +0100

    first commit

---
+++ project1/blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+
+# Categories
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=300)
+
+# Articles
+class Article(models.Model):
+    categories = models.ManyToManyField(Category, blank=True, null=True)
+    name = models.CharField(max_length=500)
+    description = models.TextField()
+
+
